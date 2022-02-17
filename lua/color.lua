302b8872384885244ec613c27307a8e8ff826e13
Ivan Oreshnikov <oreshnikov.ivan@gmail.com>
2022-02-17 23:18:27 +0100

    Extracted the color palette into a separate module

---
+++ lua/color.lua
@@ -0,0 +1,20 @@
+local hsl = require('lush.hsl')
+
+return {
+    base03  = hsl("#002b36"),
+    base02  = hsl("#073642"),
+    base01  = hsl("#586e75"),
+    base00  = hsl("#657b83"),
+    base0   = hsl("#839496"),
+    base1   = hsl("#93a1a1"),
+    base2   = hsl("#eee8d5"),
+    base3   = hsl("#fdf6e3"),
+    yellow  = hsl("#b58900"),
+    orange  = hsl("#cb4b16"),
+    red     = hsl("#dc322f"),
+    magenta = hsl("#d33682"),
+    violet  = hsl("#6c71c4"),
+    blue    = hsl("#268bd2"),
+    cyan    = hsl("#2aa198"),
+    green   = hsl("#859900"),
+}
--- lua/lush_theme/solarized.lua
+++ lua/lush_theme/solarized.lua
@@ -18,28 +18,7 @@
 local lush = require('lush')
 local hsl = require('lush.hsl')
 
--- We start by defining the colorscheme. We use the original definition by
--- Ethan Schoonover's vim-colors-solarized which can be found here:
---     https://github.com/altercation/vim-colors-solarized
-
-local color = {
-    base03  = hsl("#002b36"),
-    base02  = hsl("#073642"),
-    base01  = hsl("#586e75"),
-    base00  = hsl("#657b83"),
-    base0   = hsl("#839496"),
-    base1   = hsl("#93a1a1"),
-    base2   = hsl("#eee8d5"),
-    base3   = hsl("#fdf6e3"),
-    yellow  = hsl("#b58900"),
-    orange  = hsl("#cb4b16"),
-    red     = hsl("#dc322f"),
-    magenta = hsl("#d33682"),
-    violet  = hsl("#6c71c4"),
-    blue    = hsl("#268bd2"),
-    cyan    = hsl("#2aa198"),
-    green   = hsl("#859900"),
-}
+local color = require('color')
 
 if vim.o.background == "light" then
     local temp03 = color.base03
